18f4eabeebdcfbcfc4b6d5633f04e5a98b37c0b4
seervipradhyumn04-ship-it <seervipradhyumn04@gmail.com>
2026-06-23 22:02:42 +0530

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+b = np.array([[9,8,7],[6,5,4],[3,2,1]])
+
+print("Addition:\n", a+b)
+print("Subtraction:\n", a-b)
+print("Matrix Multiplication:\n", np.dot(a,b))
